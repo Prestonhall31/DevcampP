283faf09fb9fa55c2c5546199dab629b4481fc48
prestonhall31 <prestonhall31@gmail.com>
2017-02-02 15:47:29 -0700

    Implemented ActionCable for comments: done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
-
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
@@ -19,6 +17,6 @@ Rails.application.routes.draw do
   end
 
   mount ActionCable.server => '/cable'
-  
+
   root to: 'pages#home'
 end
